3a4e32158b6724ae6b3fc3aa9ba4668ae9a6aac4
MarharytaOsypova <osipovamv03@gmail.com>
2024-10-06 18:09:47 +0300

    jshm7

---
+++ js/task-6.js
@@ -0,0 +1,42 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+ 
+      const input = document.querySelector('#controls input');
+      const createBtn = document.querySelector('[data-create]');
+      const destroyBtn = document.querySelector('[data-destroy]');
+      const boxesContainer = document.querySelector('#boxes');
+
+      createBtn.addEventListener('click', () => {
+        const amount = parseInt(input.value);
+        if (amount >= 1 && amount <= 100) {
+          createBoxes(amount);
+          input.value = '';  
+        }  
+      });
+
+      destroyBtn.addEventListener('click', destroyBoxes);
+
+      function createBoxes(amount) {
+        destroyBoxes();  
+
+        const elements = [];
+        let size = 30;  
+        for (let i = 0; i < amount; i++) {
+          const box = document.createElement('div');
+          box.style.width = `${size}px`;
+          box.style.height = `${size}px`;
+          box.style.backgroundColor = getRandomHexColor();
+          elements.push(box);  
+          size += 10;  
+        }
+
+        boxesContainer.append(...elements);  
+      }
+
+      function destroyBoxes() {
+        boxesContainer.innerHTML = '';  
+      }
